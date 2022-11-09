73a790a953f1ec13e90e02fd97b1b39064ad6225
Nicolas Martin Caceres Sala <martin.sala@cencosud.com>
2022-11-09 17:23:53 -0300

    Create method, find, findOne and remove in products.service.ts

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,6 +3,7 @@ import {
   Injectable,
   InternalServerErrorException,
   Logger,
+  NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -33,20 +34,29 @@ export class ProductsService {
     }
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    return await this.productRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    // Otra forma de hacer lo mismo
+    // return await this.productRepository.find({where: { id }});
+    // this.productRepository.findOne({where: {id}})
+    const product = await this.productRepository.findOneBy({ id });
+    if (!product) {
+      throw new NotFoundException('No existe producto con ese ID!');
+    }
+    return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const product = await this.findOne(id)
+    await this.productRepository.remove(product)
+    return product;
   }
 
   // Metodo privado para manejar excepciones
